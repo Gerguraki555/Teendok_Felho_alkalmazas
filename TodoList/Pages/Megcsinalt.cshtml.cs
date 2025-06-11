9e84fe69443f55a29ef7c38fe840492c28641584
Gerguraki555 <gergurakki@gmail.com>
2025-06-11 18:41:58 +0200

    Mostmar latjuk a megcsinalt teendoket

---
+++ TodoList/Pages/Megcsinalt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TodoList.Pages
+{
+    public class MegcsinaltakModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
